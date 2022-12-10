62beef2e2cff33d5ea3b00aee30e847ed635599a
Duy Dinh Quoc <dinhquocduy5@gmail.com>
2022-12-10 21:36:45 +0700

    init src

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("HI EXPRESS.JS");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const UserRoutes = require("./routes/UserRoutes");
+
+let bodyParser = require("body-parser");
+
+const app = express();
+
+dotenv.config();
+mongoose.connect(
+  process.env.MONGODB_CONFIG,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected!!!");
+  }
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+app.use(morgan("common"));
+
+app.use("/user", UserRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Connect success on ${PORT} !!!`);
+});
